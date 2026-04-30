24a482a7789f5593a280b5e6c1c1b9ac719b0ddc
ritika rawat <ritikarawat07@users.noreply.github.com>
2026-04-30 22:03:26 +0530

    Allow VITE_API_URL to override hardcoded localhost

--- frontend/src/services/http.js
+++ frontend/src/services/http.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { authStorage } from '../utils/authStorage'
 
 const http = axios.create({
-    baseURL: 'http://127.0.0.1:8000/api',
+    baseURL: import.meta.env.VITE_API_URL || 'http://127.0.0.1:8000/api',
 })
 
 http.interceptors.request.use((config) => {
